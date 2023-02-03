e77a3cf36952292e3b76077f72a5cd2fd92ea363
Aryan20509 <121623383+Aryan20509@users.noreply.github.com>
2023-02-03 11:21:17 +0530

    update binary.py

--- binary.py
+++ binary.py
@@ -9,7 +9,7 @@ def binary_search(arr, low, high, x):
           return binary_search(arr,  mid + 1, high, x)
     else:
         return - 1
-arr = [2, 3, 4,10,40]
+arr = [4,6, 56,10,50]
 x=10
 result = binary_search(arr, 0, len(arr)-1, x)
 if result != -1:
